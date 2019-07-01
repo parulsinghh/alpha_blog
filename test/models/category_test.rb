c7a1b33f1f10559ff70b1de6c9b1327d130d5b4b
Parul Singh <parul.rhl@gmail.com>
2019-07-01 17:04:19 +0100

    Automated testing for category

---
+++ test/models/category_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+  def setup
+    @category = Category.new(name: 'sports')
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    refute @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    category2 = Category.new(name: "sports")
+    assert_not category2.valid?
+  end
+
+  test "name should not be long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "aa"
+    refute @category.valid?
+  end
+end
